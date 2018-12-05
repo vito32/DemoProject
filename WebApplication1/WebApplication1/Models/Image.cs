8dc72a01c3becbc168f7998f5f3314e808229e9a
vito32 <32vito@gmail.com>
2018-12-05 15:47:47 +0100

    Add images in carousel(4) and about

---
+++ WebApplication1/WebApplication1/Models/Image.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class Image
+    {
+        public Image(string path, string desc)
+        {
+            Path = path;
+            Description = desc;
+
+        }
+        public string Path { get; set; }
+        public string Description { get; set; }
+    }
+}
